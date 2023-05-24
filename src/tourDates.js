be1457f22a66eeb507ff486a72d1f93a5766f712
Austin Flowers <austin.flowers10@gmail.com>
2023-05-24 11:41:11 -0500

    Project Setup

---
+++ src/tourDates.js
@@ -0,0 +1,17 @@
+/* 
+  Responsbility:
+
+  This module should contain a function that converts
+  the raw data objects for tour dates into HTML elements 
+  with the following structure.
+
+  <article class="events">
+    <header>Tour Details</header>
+
+    <ol>
+      <li class="event">City, Country on Date:Time at Venue with opening act Act</li>
+      <li class="event">City, Country on Date:Time at Venue with opening act Act</li>
+      <li class="event">City, Country on Date:Time at Venue with opening act Act</li>
+    </ol>
+  </article>
+*/
